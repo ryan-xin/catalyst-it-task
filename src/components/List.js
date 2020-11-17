b71ca419ca71bd6b86ef3f711408562b37f9f86d
Ryan Xin <xinpf0715@gmail.com>
2020-11-17 20:15:58 +1100

    Profile & List components created

---
+++ src/components/List.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const List = () => {
+  return (
+    <div>
+      <h2>List</h2>
+    </div>
+  )
+};
+
+export default List;
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Profile = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  )
+};
+
+export default Profile;
